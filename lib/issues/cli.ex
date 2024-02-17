de3aa41938a171eb9112b9feef08b19f5b73f883
b13v <yba@proton.me>
2024-02-17 20:26:25 +0600

    Refining and optimization of fetching and handle errors in module

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -21,22 +21,22 @@ defmodule Issues.CLI do
 
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
-  """
-    def parse_args(argv) do
-      parse =
-        OptionParser.parse(argv,
-          switches: [help: :boolean],
-          aliases: [h: :help]
-        )
 
-      case parse do
-        {[help: true], _, _} -> :help
-        {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
-        {_, [user, project], _} -> {user, project, @default_count}
-        _ -> :help
-      end
-    end
-  """
+  #  def parse_args(argv) do
+  #    parse =
+  #      OptionParser.parse(argv,
+  #        switches: [help: :boolean],
+  #        aliases: [h: :help]
+  #      )
+  #
+  #   case parse do
+  #      {[help: true], _, _} -> :help
+  #      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
+  #      {_, [user, project], _} -> {user, project, @default_count}
+  #      _ -> :help
+  #    end
+  #  end
+  #
 
   def parse_args(argv) do
     OptionParser.parse(argv,
@@ -70,5 +70,13 @@ defmodule Issues.CLI do
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
   end
 end
